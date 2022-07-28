65bd809a81bb3ebdedb57e98cbf52894b525dcf0
Nospher <jooownyy@gmail.com>
2022-07-28 03:34:43 -0300

    added patient change password

--- app/Controllers/Http/PatientsController.ts
+++ app/Controllers/Http/PatientsController.ts
@@ -1,45 +1,64 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Patient from 'App/Models/Patient';
+import bcrypt from 'bcrypt';
 
 export default class PatientsController {
-  public async index({}: HttpContextContract) {}
+  public async index({ }: HttpContextContract) { }
 
-  public async store({request, response}: HttpContextContract) {
+  public async store({ request, response }: HttpContextContract) {
     const data = request.only(['fullName', 'email', 'birthDate', 'genderId']);
-    const patientExists = await Patient.find({email: data.email});
+    const patientExists = await Patient.find({ email: data.email });
 
-    if(patientExists) {
-      return response.status(422).json({error: 'This email has already been taken.'});
+    if (patientExists) {
+      return response.status(422).json({ error: 'This email has already been taken.' });
     }
-    
+
     const result = await Patient.create(data);
     return response.status(201).json(result);
   }
 
-  public async show({request, response}: HttpContextContract) {
+  public async show({ request, response }: HttpContextContract) {
     const id = request.param('id');
     const patient = await Patient.find(id);
-    
-    if(!patient) {
-      return response.status(404).send({error: 'Patient not found.'});
+
+    if (!patient) {
+      return response.status(404).send({ error: 'Patient not found.' });
     }
 
     return response.status(200).send(patient);
   }
 
-  public async update({request, response}: HttpContextContract) {
+  public async update({ request, response }: HttpContextContract) {
     const patient = await Patient.find(request.param('id'));
 
-    if(!patient) {
-      return response.status(404).send({error: 'Patient not found.'});
+    if (!patient) {
+      return response.status(404).send({ error: 'Patient not found.' });
     }
 
-    await patient.merge(request.all()).save();
+    let data = request.all();
 
+    const { password, oldPassword, confirmPassword } = request.only(['password', 'oldPassword', 'confirmPassword']);
+    if (password && oldPassword && confirmPassword) {
+      const equalsPassword = await bcrypt.compare(oldPassword, patient.password);
+      if (!equalsPassword) {
+        return response.status(422).send({ error: "Old password is wrong." });
+      }
+
+      if (!(password === confirmPassword)) {
+        return response.status(422).send({ error: "Password and confirm password are different." });
+      }
+
+      delete data.oldPassword;
+      delete data.confirmPassword;
+
+      data.password = await bcrypt.hash(password, 10);
+    }
+
+    await patient.merge(data).save();
     return response.status(200).send(true);
   }
 
-  public async destroy({request, response}: HttpContextContract) {
+  public async destroy({ request, response }: HttpContextContract) {
     const patient = await Patient.find(request.param('id'));
     await patient?.delete();
     return response.status(204);
