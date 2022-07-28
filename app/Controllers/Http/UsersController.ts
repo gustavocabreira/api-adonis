0d843333561fea6a89dc66351e1878044796946d
Gustavo Cabreira <gustavo.softdev@gmail.com>
2022-07-28 04:18:52 -0300

    added update user feature

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,5 +1,6 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User';
+import bcrypt from 'bcrypt';
 
 export default class UsersController {
   public async index({}: HttpContextContract) {}
@@ -27,7 +28,37 @@ export default class UsersController {
     return response.status(200).json(user);
   }
 
-  public async update({}: HttpContextContract) {}
+  public async update({request, response}: HttpContextContract) {
+    const id = request.param('id');
+    const user = await User.find(id);
+    
+    if (!user) {
+      return response.status(404).send({ error: 'User not found.' });
+    }
+    
+    let data = request.all();
+    const { password, oldPassword, confirmPassword } = request.only(['password', 'oldPassword', 'confirmPassword']);
+    
+    if (password && oldPassword && confirmPassword) {
+      const equalsPassword = await bcrypt.compare(oldPassword, user.password);
+      if (!equalsPassword) {
+        return response.status(422).send({ error: "Old password is wrong." });
+      }
+
+      if (!(password === confirmPassword)) {
+        return response.status(422).send({ error: "Password and confirm password are different." });
+      }
+
+      delete data.oldPassword;
+      delete data.confirmPassword;
+
+      data.password = await bcrypt.hash(password, 10);
+    }
+
+    await user.merge(data).save();
+
+    return response.status(200).send(true);
+  }
 
   public async destroy({}: HttpContextContract) {}
 }
