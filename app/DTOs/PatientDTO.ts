4d21cc4db92add1255125c0ca01b30b96686cd77
Gustavo Cabreira <gustavo.softdev@gmail.com>
2022-08-03 12:28:55 -0300

    fixed PatientDTO

--- app/DTOs/PatientDTO.ts
+++ app/DTOs/PatientDTO.ts
@@ -1,6 +1,5 @@
 import { IUser } from '../Interfaces/IUser';
 import { v4 as uuidv4 } from 'uuid';
-import { DateTime } from 'luxon';
 
 export class PatientDTO implements IUser {
     fullName: string;
